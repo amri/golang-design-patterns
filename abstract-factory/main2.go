9a893967fe9ae9db7613647b2b78391529e5ca04
radiation70 <radiation70@protonmail.com>
2022-07-08 08:38:40 +0800

    abstract factory modif

--- abstract-factory/main2.go
+++ abstract-factory/main2.go
@@ -5,6 +5,7 @@ import "fmt"
 type ILaptop interface {
 	setMemory(amount int)
 	getMemory() int
+	getBrand() string
 }
 
 type AbstractLaptopFactory interface {
@@ -12,6 +13,7 @@ type AbstractLaptopFactory interface {
 }
 
 type laptop struct {
+	brand  string
 	memory int
 }
 
@@ -27,12 +29,17 @@ func (al *AppleLaptop) getMemory() int {
 	return al.memory
 }
 
+func (al *AppleLaptop) getBrand() string {
+	return al.brand
+}
+
 type AppleLaptopFactory struct {
 }
 
 func (a *AppleLaptopFactory) createLaptop() ILaptop {
 	return &AppleLaptop{
 		laptop{
+			brand:  "Apple",
 			memory: 128,
 		},
 	}
@@ -44,6 +51,7 @@ type WindowsLaptopFactory struct {
 func (w *WindowsLaptopFactory) createLaptop() ILaptop {
 	return &WindowsLaptop{
 		laptop{
+			brand:  "Windows",
 			memory: 256,
 		},
 	}
@@ -59,6 +67,10 @@ func (al *WindowsLaptop) setMemory(amount int) {
 
 func (al *WindowsLaptop) getMemory() int {
 	return al.memory
+}
+
+func (al *WindowsLaptop) getBrand() string {
+	return al.brand
 }
 
 func getLaptopFactory(brand string) AbstractLaptopFactory {
@@ -78,7 +90,8 @@ func main() {
 
 	appleLaptop1 := appleFactory.createLaptop()
 	appleLaptop2 := appleFactory.createLaptop()
+	appleLaptop2.setMemory(200)
 	windowsLaptop := windowsFactory.createLaptop()
 
-	fmt.Println(appleLaptop1.getMemory(), appleLaptop2.getMemory(), windowsLaptop.getMemory())
+	fmt.Println(appleLaptop1.getBrand(), appleLaptop1.getMemory(), appleLaptop2.getMemory(), windowsLaptop.getBrand(), windowsLaptop.getMemory())
 }
